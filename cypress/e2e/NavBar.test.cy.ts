3989f7465aba3e21f143b9bea4c79215510842cf
Gustavo Silva <gustavo.silva.nav@gmail.com>
2025-06-22 13:31:34 -0300

    🎨 Adding some cypress testing

---
+++ cypress/e2e/NavBar.test.cy.ts
@@ -0,0 +1,25 @@
+describe('NavBar Component test', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('Should have a home title', () => {
+    cy.get('[data-testid="cy-home-route"]').find('h3').should('exist').should('have.text', 'GSN');
+    cy.get('[data-testid="cy-home-route"]').find('img').should('exist').should('be.visible');
+    cy.get('[data-testid="cy-home-route"]').find('img').should('have.attr', 'alt', 'Logo');
+  });
+
+  it('Should have a utility section', () => {
+    cy.get('[data-testid="cy-utilities-route"]').should('have.attr', 'href', '/utilities');
+    cy.get('[data-testid="cy-utilities-route"]').should('contain', 'Utilities');
+    cy.get('[data-testid="cy-utilities-route"]').click();
+    cy.url().should('include', '/utilities');
+  });
+
+  it('Should have a tariff router', () => {
+    cy.get('[data-testid="cy-tariffs-route"]').should('have.attr', 'href', '/tariffs');
+    cy.get('[data-testid="cy-tariffs-route"]').should('contain', 'Tariffs');
+    cy.get('[data-testid="cy-tariffs-route"]').click();
+    cy.url().should('include', '/tariffs');
+  });
+});
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ export const NavBar = () => {
   return (
     <header className="h-screen flex-[0.2_1_0%] min-w-[280px] bg-[#020712] text-white">
       <nav className="py-4">
-        <Link to="/" className="flex items-center justify-center p-4 cursor-pointer mb-2">
+        <Link to="/" data-testid="cy-home-route" className="flex items-center justify-center p-4 cursor-pointer mb-2">
           <img src={gsnIcon} alt="Logo" className="w-[20%]" />
           <h3 className="text-3xl ml-3">GSN</h3>
         </Link>
@@ -15,12 +15,14 @@ export const NavBar = () => {
         <div>
           <Link
             to="/utilities"
+            data-testid="cy-utilities-route"
             className="flex items-center justify-center p-4 transition-all duration-300 ease-in-out cursor-pointer hover:bg-white/10 group-hover:blur-sm hover:!blur-none">
             <ApartmentRoundedIcon />
             <h3 className="text-xl ml-2">Utilities</h3>
           </Link>
           <Link
             to="/tariffs"
+            data-testid="cy-tariffs-route"
             className="flex items-center justify-center p-4 transition-all duration-300 ease-in-out cursor-pointer hover:bg-white/10 group-hover:blur-sm hover:!blur-none">
             <InsertDriveFileRoundedIcon />
             <h3 className="text-xl ml-2">Tariffs</h3>
