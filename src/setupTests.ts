77846659c5e6044f81780c3e8d8dff4a1ed3a1fd
Gustavo Silva <gustavo.silva.nav@gmail.com>
2025-06-22 21:03:53 -0300

    🔥 Removing everything related to msw

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,1 @@
 import '@testing-library/jest-dom/vitest';
-import { server } from './mockServer/server';
-
-beforeAll(() => server.listen());
-
-afterEach(() => server.resetHandlers());
-
-afterAll(() => server.close());
