e877e195c400921ec8e502ec7383722c143fda58
Gustavo Silva <gustavo.silva.nav@gmail.com>
2025-06-28 16:45:06 -0400

    ♻️ Refactoring and making the information pages for tariffs and utilities
    more reusable

--- src/components/AllTariffDetails.tsx
+++ src/components/AllTariffDetails.tsx
@@ -1,17 +1,8 @@
-import {
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableFooter,
-  TablePagination,
-  TableRow,
-  Table,
-  TableHead,
-  Paper,
-} from '@mui/material';
+import { TableBody, TableCell, TableRow } from '@mui/material';
 import { tariffHeaderRows } from '@/utils';
 import { useState } from 'react';
 import { useGetAllTariffDataQuery } from '@/state/genability/genabilitySlice';
+import { TableComponent } from './Table/TableComponent';
 
 export const AllTariffDetails = () => {
   const [page, setPage] = useState(0);
@@ -20,12 +11,16 @@ export const AllTariffDetails = () => {
   const { data, isLoading, isError } = useGetAllTariffDataQuery({ pageCount: rowsPerPage, pageStart: fetchPage });
 
   const handleChangePage = (_e: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+    if (!data) return;
+
     const nextPage = newPage * rowsPerPage;
     setPage(newPage);
     setFetchPage(nextPage);
   };
 
   const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!data) return;
+
     setRowsPerPage(+event.target.value);
     setFetchPage(0);
     setPage(0);
@@ -39,7 +34,7 @@ export const AllTariffDetails = () => {
     );
   }
 
-  if (isError) {
+  if (isError || !data) {
     return (
       <div>
         <p>Error...</p>
@@ -48,54 +43,24 @@ export const AllTariffDetails = () => {
   }
 
   return (
-    <div className="my-10 mx-6">
-      <h1 className="text-center text-4xl font-bold text-[#020712] pb-8">List of Tariffs</h1>
-
-      {data ? (
-        <TableContainer component={Paper} sx={{ maxHeight: '500px' }}>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow
-                sx={{
-                  '& th': {
-                    color: '#fff',
-                    fontSize: '18px',
-                    backgroundColor: '#121212',
-                  },
-                }}>
-                {tariffHeaderRows.map((row) => (
-                  <TableCell key={row.id} align={row.align}>
-                    {row.name}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.results.map((tariff) => (
-                <TableRow key={tariff.masterTariffId} hover sx={{ cursor: 'pointer' }}>
-                  <TableCell>{tariff.tariffName}</TableCell>
-                  <TableCell>{tariff.tariffCode}</TableCell>
-                  <TableCell>{tariff.masterTariffId}</TableCell>
-                  <TableCell>{tariff.lseName}</TableCell>
-                  <TableCell>{tariff.effectiveDate}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TablePagination
-                  rowsPerPageOptions={[10, 25, 100, 200]}
-                  count={data.count}
-                  rowsPerPage={data.pageCount}
-                  page={page}
-                  onPageChange={handleChangePage}
-                  onRowsPerPageChange={handleChangeRowsPerPage}
-                />
-              </TableRow>
-            </TableFooter>
-          </Table>
-        </TableContainer>
-      ) : null}
-    </div>
+    <TableComponent
+      title="List of Tariffs"
+      data={data}
+      page={page}
+      headerRows={tariffHeaderRows}
+      handleChangePage={handleChangePage}
+      handleChangeRowsPerPage={handleChangeRowsPerPage}>
+      <TableBody>
+        {data.results.map((tariff) => (
+          <TableRow key={tariff.masterTariffId} hover sx={{ cursor: 'pointer' }}>
+            <TableCell>{tariff.tariffName}</TableCell>
+            <TableCell>{tariff.tariffCode}</TableCell>
+            <TableCell>{tariff.masterTariffId}</TableCell>
+            <TableCell>{tariff.lseName}</TableCell>
+            <TableCell>{tariff.effectiveDate}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </TableComponent>
   );
 };
--- src/components/Utility.tsx
+++ src/components/Utility.tsx
@@ -1,14 +1,9 @@
 import { useState } from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import { utilityRows } from '@/utils';
+import { TableBody, TableCell, TableRow } from '@mui/material';
+import { TableComponent } from '@/components/Table/TableComponent';
 import { useGetAllUtilityDataQuery } from '@/state/genability/genabilitySlice';
+import { utilityRows } from '@/utils';
+
 import './utility.css';
 
 export const Utility = () => {
@@ -18,12 +13,16 @@ export const Utility = () => {
   const { data, isLoading, isError } = useGetAllUtilityDataQuery({ pageStart: fetchPage, pageCount: rowsPerPage });
 
   const handleChangePage = (_e: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+    if (!data) return;
+
     const nextPage = newPage * rowsPerPage;
     setPage(newPage);
     setFetchPage(nextPage);
   };
 
   const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!data) return;
+
     setRowsPerPage(+event.target.value);
     setFetchPage(0);
     setPage(0);
@@ -33,61 +32,36 @@ export const Utility = () => {
     return <div>Loading...</div>;
   }
 
-  if (isError) {
+  if (isError || !data) {
     return <div>Error...</div>;
   }
 
   return (
-    <div className="my-10 mx-6">
-      <h1 data-testid="cy-utilities-title" className="text-center text-4xl font-bold text-[#020712] pb-8">
-        List of Utilities
-      </h1>
-
-      {data ? (
-        <Paper sx={{ width: '100%', overflow: 'hidden', borderRadius: '8px' }} data-testid="cy-utilities-table-data">
-          <TableContainer className="tableContainer">
-            <Table stickyHeader aria-label="sticky table">
-              <TableHead>
-                <TableRow sx={{ '& th': { color: '#fff', fontSize: '18px', backgroundColor: '#121212' } }}>
-                  {utilityRows.map((row) => (
-                    <TableCell key={row.id} align={row.align}>
-                      {row.name}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {data.results.map((lse) => (
-                  <TableRow
-                    data-testid={`cy-row-${lse.lseId}`}
-                    hover
-                    role="checkbox"
-                    tabIndex={-1}
-                    key={lse.lseId}
-                    sx={{ cursor: 'pointer' }}>
-                    <TableCell className="min-w-xs">{lse.name}</TableCell>
-                    <TableCell>{lse.lseCode}</TableCell>
-                    <TableCell className="max-w-xs break-words">{lse.websiteHome}</TableCell>
-                    <TableCell align="center">
-                      {lse.totalCustomers !== null ? lse.totalCustomers.toLocaleString('en-US') : null}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            data-testid="cy-utilities-pagination"
-            rowsPerPageOptions={[10, 25, 100, 200]}
-            component="div"
-            count={data.count}
-            rowsPerPage={data.pageCount}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-          />
-        </Paper>
-      ) : null}
-    </div>
+    <TableComponent
+      headerRows={utilityRows}
+      data={data}
+      page={page}
+      handleChangePage={handleChangePage}
+      handleChangeRowsPerPage={handleChangeRowsPerPage}
+      title="List of Utilities">
+      <TableBody>
+        {data.results.map((lse) => (
+          <TableRow
+            data-testid={`cy-row-${lse.lseId}`}
+            hover
+            role="checkbox"
+            tabIndex={-1}
+            key={lse.lseId}
+            sx={{ cursor: 'pointer' }}>
+            <TableCell className="min-w-xs">{lse.name}</TableCell>
+            <TableCell>{lse.lseCode}</TableCell>
+            <TableCell className="max-w-xs break-words">{lse.websiteHome}</TableCell>
+            <TableCell align="center">
+              {lse.totalCustomers !== null ? lse.totalCustomers.toLocaleString('en-US') : null}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </TableComponent>
   );
 };
